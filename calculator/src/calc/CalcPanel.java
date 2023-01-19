1618c611d7eca8611238fbaeb9d43f5e4ff90229
вова <karve123789@gmail.com>
2023-01-19 10:30:25 +0300

    попробовал пользоваться регулярками

--- calculator/src/calc/CalcPanel.java
+++ calculator/src/calc/CalcPanel.java
@@ -7,7 +7,7 @@ public class CalcPanel {
   public static void newPanel(){
     Scanner sc = new Scanner(System.in);
     // 4 + 5 или например 4 + IV
-    String raw = sc.nextLine();
+      String raw = sc.nextLine();
 
     // тут мы понимаем уже какой был введен оператор
     String operator = Parser.parseOperator(raw);
--- calculator/src/calc/Parser.java
+++ calculator/src/calc/Parser.java
@@ -1,11 +1,18 @@
 package calc;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Parser {
   private static int[] digits = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   private static String[] rimDigits = {"I", "II", "III", "IV"};
   public static String parseOperator(String message){
     // [+ - * /]
     // регулярные выражения
+    Pattern pt1 = Pattern.compile("d");
+    Pattern pt2 =Pattern.compile("I,II,III,IV,VI,VII,VIII,IX,X");
+    Matcher m1= pt1.matcher(message);//нужно считать со сканера
+    Matcher m2 = pt2.matcher(message);
     if(message.contains("+")){
       return "+";
     }
